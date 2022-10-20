dd760e5c53e87ac90f99af075332cf1e75abb1d5
Grace Ocampina <grace.ocampina@ucsf.edu>
2022-10-20 14:16:00 -0700

    simplified the codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//possible password variables and values
+//possible variables and values
+var charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericSet = ["0","1","2","3","4","5","6","7","8","9"];
+var symbols = "!@#$%-+=/^&";
+
+
 
 function generatePassword () {
-  alert("You are about to generate a password.\nPress ok to continue ");
+
+//Prompts user about the criteria in generating password
+var passwordLength = prompt("You are about to generate a password.\nChoose the number of your password length.\nMust be 8 - 128 long");
+
+//if user cancels
+if (!passwordLength) {
+    return;
+}
+// if user entered letters which is invalid then prompts to enter number
+if (!passwordLength.charSet) {
+    prompt("Invalid number! Enter number between 8 -128");
+    return;
+
+} 
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Too short");
+  return generatePassword(); 
+}
+
+}
+
+
+
+/*
+function generatePassword() {
+    alert("You are about to generate password.\nPress 'Ok' to continue");
 
 var passwordLength = prompt("Choose the length of you password.\nEnter number from 8 - 128 long");
 var passwordLengthResult = {
@@ -12,16 +43,8 @@ var passwordLengthResult = {
   numericList : "0123456789",
   symbols : "!@#$%-+=/^&"
 
-};
-
-if
-
-  
-
-}
-  
-  
-
+  }
+};*/
 
 
 
@@ -38,5 +61,4 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", writePassword); 
